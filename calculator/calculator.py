ed68f6a7f663246cf474011543f7138bf721ef38
aayushkharel01 <aayushkharel4369@gmail.com>
2024-11-20 13:35:08 +0545

    added functionality

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -23,21 +23,43 @@ buttons = [
     ]
 
 clear = QPushButton("C")
-delete = QPushButton("<")
+delete = QPushButton("Delete")
+
+def button_click():
+    button = app.sender()
+    text = button.text()
+
+    if text =="=":
+        symbol = text_box.text()
+        try:
+            res = eval(symbol)
+            text_box.setText(str(res))
+
+        except Exception as e:
+            print("Error:", e)
+
+    elif text == "C":
+        text_box.clear()
+
+    elif text == "Delete":
+        current_value = text_box.text()
+        text_box.setText(current_value[:-1])
+    else:
+        current_value = text_box.text()
+        text_box.setText(current_value + text)
+        
+
 
 row = 0
 col = 0
 for text in buttons:
     button = QPushButton(text)
-    #button.clicked.connect(None)
+    button.clicked.connect(button_click)
     grid.addWidget(button, row, col)
     col += 1
     if col >3:
         col = 0
         row +=1
-        
-
-
 
 #Design
 
@@ -49,11 +71,13 @@ button_row = QHBoxLayout()
 button_row.addWidget(clear)
 button_row.addWidget(delete)
 
-
 master_layout.addLayout(button_row)
-
 main_window.setLayout(master_layout)
 
+clear.clicked.connect(button_click)
+delete.clicked.connect(button_click)
+
 #Show/Run
+
 main_window.show()
 app.exec_()
