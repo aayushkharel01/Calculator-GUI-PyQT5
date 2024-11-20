5cd9d65b905fd93a7efa52a74c6739c410032061
aayushkharel01 <aayushkharel4369@gmail.com>
2024-11-20 12:56:02 +0545

    Template to start our Calculator App

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,1 +1,25 @@
+#imports
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QGridLayout, QLineEdit, QWidget, QPushButton, QVBoxLayout, QHBoxLayout
+
+#App settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Calculator App")
+main_window.resize(250, 300)
+
+#All objects/widgets
+
+text_box = QLineEdit()
+grid = QGridLayout()
+
+
+#Design
+
+
+
+
+#Show/Run
+main_window.show()
+app.exec_()
